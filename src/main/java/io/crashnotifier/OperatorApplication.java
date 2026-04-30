a73c8056640cf93b01a71506f069473249f879c8
thallesf <thallesf@ciandt.com>
2026-04-30 14:10:02 -0300

    feat: scaffold Maven project with JOSDK dependencies

---
+++ src/main/java/io/crashnotifier/OperatorApplication.java
@@ -0,0 +1,19 @@
+package io.crashnotifier;
+
+import io.javaoperatorsdk.operator.Operator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+
+public class OperatorApplication {
+    private static final Logger log = LoggerFactory.getLogger(OperatorApplication.class);
+
+    public static void main(String[] args) {
+        log.info("Starting Pod Crash Notifier Operator");
+        Operator operator = new Operator();
+        operator.installShutdownHook(Duration.ofSeconds(10));
+        operator.start();
+        log.info("Operator started successfully");
+    }
+}
